37be720197073fc6d0ac2a4c4ed8a6ad18919b39
isabellamoleski <112529169+isabellamoleski@users.noreply.github.com>
2022-10-25 15:08:24 -0600

    Add files via upload

---
+++ project-stage-3/Item.java
@@ -0,0 +1,36 @@
+public class Item {
+    private String name;
+    private double cost;
+    private int itemNumber;
+    //constructors
+    public Item(){
+        name = " ";
+        cost = 0;
+        itemNumber = 0;
+    }
+    public Item(String s, double d, int i){
+        name = s;
+        cost = d;
+        itemNumber = i;
+    }
+    //get methods
+    public double getCost(){
+        return cost;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getNumber(){
+        return itemNumber;
+    }
+    //set methods
+    public void setName(String s){
+        name = s;
+    }
+    public void setCost(double d){
+        cost = d;
+    }
+    public void setNumber(int i){
+        itemNumber = i;
+    }
+}
