d4f99d5e8b4aae8d2087d3734ae921201f5df6d5
isabellamoleski <112529169+isabellamoleski@users.noreply.github.com>
2022-11-09 17:49:08 -0700

    Update Menuu.java

--- project-stage-3/Menuu.java
+++ project-stage-3/Menuu.java
@@ -8,6 +8,7 @@ public class Menuu {
     private static LinkedList<Customer> customers = new LinkedList<>();
     private static LinkedList<Item> items = new LinkedList<>();
     private static Sale newSale = new Sale();
+    private static Financial teaShop = new Financial();
     
     public static void main(String[] args) {
         //dummy employee info
@@ -63,7 +64,7 @@ public class Menuu {
                 SaleMenu();
                 break;
             case 5: 
-                //ReportsMenu
+                ReportsMenu();
                 break;
             case 6: 
                 ItemsMenu();
@@ -534,6 +535,7 @@ public class Menuu {
                 System.out.println("Is this customer a member? (true/false):");
                 boolean mem = scan.nextBoolean();
                 System.out.println("Total Cost: "+newSale.getTotalCost(mem));
+                teaShop.addToTotal(newSale.getTotalCost(mem));
                 newSale.order.clear();
                 SaleMenu();
                 break;
@@ -557,7 +559,16 @@ public class Menuu {
         int option = scan.nextInt();
         switch(option){
             case 1: //report of total profit
-                //System.out.print(Financial.getTotalProfit());
+                System.out.println("Total Profits Report \n--------------------");
+                ArrayList<Double> allSales = new ArrayList<>();
+                allSales = teaShop.getTotalProfit();
+                double totalProf=0;
+                for(int i=0;i<allSales.size();i++){ //prints all sales and how much they were
+                    System.out.println("Sale of: $"+allSales.get(i));
+                    totalProf=totalProf+allSales.get(i);
+                }
+                //prints total profit
+                System.out.println("With a total profit of: "+totalProf);
                 ReportsMenu();
                 break;
             case 2: //Report on total expenses
