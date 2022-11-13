5d71d530d9c366c98e6d5147d7ebefbaecef7653
isabellamoleski <112529169+isabellamoleski@users.noreply.github.com>
2022-11-13 02:40:09 -0700

    Update Financial.java

--- project-stage-3/Financial.java
+++ project-stage-3/Financial.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 public class Financial {
     ArrayList<Employee> Pay;
-    private static ArrayList<Double> totalProfit = new ArrayList<>(); 
+    private static ArrayList expenses;
     private static double totalExpenses;
     // Constructor
     public Financial(){
     }
-    public void addToTotal(double d){
-        totalProfit.add(d);
-    }
     //getter
-    public static ArrayList getTotalProfit(){
-        return totalProfit;
+    public static void getTotalProfit(ArrayList<Double> d){
+        for(int i=0; i<d.size();i++){
+            System.out.println("Sale of: "+ d.get(i));
+            totalExpenses =totalExpenses+(double)d.get(i);
+        }
+        System.out.println("With a total of: "+totalExpenses);
     }
     public static double getTotalExpenses(ArrayList<Employee> Pay){
         for(int i = 0; i<Pay.size();i++){
